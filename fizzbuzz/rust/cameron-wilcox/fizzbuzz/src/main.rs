a0885b4eb055eabb5c76590201109e5fb8ec2fe7
cameronwilcox <cjwilcox15@gmail.com>
2023-02-21 21:12:17 -0700

    Add fizzbuzz code

--- fizzbuzz/rust/cameron-wilcox/fizzbuzz/src/main.rs
+++ fizzbuzz/rust/cameron-wilcox/fizzbuzz/src/main.rs
@@ -1,7 +1,45 @@
-use std::env;
+use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args : Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("Need to have proper amount of arguments!");
+        println!("Usage: cargo run {} -- <num>", args[0]);
+        process::exit(1);
+    }
+    let up_to : i32 = args[1].parse::<i32>().unwrap();
+    for x in 1..up_to+1 {
+        
+        if is_divisible_by_three(x) {
+            print!("Fizz");
+        }
+        if is_divisible_by_five(x) {
+            print!("Buzz");
+        }
+        if !is_divisible_by_five(x) && !is_divisible_by_three(x) {
+            print!("{}", x);
+        }
+        println!();
+    }
+}
 
-    println!("My path is {}", args[0]);
+
+// adding way too many functions because I'm trying rust out for the first time
+
+fn is_divisible_by_three(x : i32) -> bool {
+    if x % 3 == 0 {
+        true
+    }
+    else {
+        false
+    }
+}
+
+fn is_divisible_by_five(x : i32) -> bool {
+    if x % 5 == 0 {
+        true
+    }
+    else {
+        false
+    }
 }
