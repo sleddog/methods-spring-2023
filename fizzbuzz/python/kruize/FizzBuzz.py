25f315ff7bb703ae18bc7bda95ef87156b39a583
Kruize Derek Christensen <christensen.kruize@gmail.com>
2023-02-28 16:29:03 -0700

    fixed some comments

--- fizzbuzz/python/kruize/FizzBuzz.py
+++ fizzbuzz/python/kruize/FizzBuzz.py
@@ -3,16 +3,16 @@ import sys
 myInput = int(sys.argv[1])
 n = 1
 
-while n < (myInput+1):#runs the loop till it gets to your number
-    if ((n % 3 == 0) and (n % 5 == 0)):#gets both conditionals 
+while n < (myInput+1):                      #runs the loop till it gets to your number
+    if ((n % 3 == 0) and (n % 5 == 0)):     #gets both conditionals 
         print ('FizzBuzz')
-    elif (n % 5 == 0):#gets just multiple of 5
+    elif (n % 5 == 0):                      #gets just multiple of 5
         print ('Buzz')
-    elif (n % 3 == 0):#gets multiple of 3
+    elif (n % 3 == 0):                      #gets multiple of 3
         print ('Fizz')
-    else:#just prints the numbers that don't meet a conditional 
+    else:                                   #just prints the numbers that don't meet a conditional 
         print (n)
 
         
-    n += 1 #increases the number each time in the while loop
+    n += 1                                  #increases the number each time in the while loop
 
