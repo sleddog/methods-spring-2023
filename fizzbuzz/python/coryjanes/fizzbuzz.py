38073da99b768b20ae4175928f8e45076298ca8a
Cory <58891657+coryjanes@users.noreply.github.com>
2023-03-21 16:22:57 -0600

    Update fizzbuzz.py

--- fizzbuzz/python/coryjanes/fizzbuzz.py
+++ fizzbuzz/python/coryjanes/fizzbuzz.py
@@ -4,15 +4,15 @@ import sys
 def fizzbuzz(total_count):
 	number =  1
 	while number <= total_count:
-		#if number is divisble by 3 and 5, display 'FizzBuzz' 
+		# if number is divisble by 3 and 5, display 'FizzBuzz' 
 		if ((number % 3 == 0) and (number % 5 == 0)):
 			sys.stdout.write("FizzBuzz")
 			print()
-		#if number is divisible by 3, display 'Fizz'
+		# if number is divisible by 3, display 'Fizz'
 		elif number % 3 == 0:
 			sys.stdout.write("Fizz")
 			print()
-		#if number is divisible by 5, display 'Buzz'
+		# if number is divisible by 5, display 'Buzz'
 		elif number % 5 == 0:
 			sys.stdout.write("Buzz")
 			print()
