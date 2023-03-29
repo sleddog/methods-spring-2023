68413b2de7c3b115a2d13c17aea4d3092f2e5475
Kruize Derek Christensen <christensen.kruize@gmail.com>
2023-03-29 10:26:54 -0600

    Added Ping and Pong Options
    Multiples of 3 should print “Fizz”
    Multiples of 5 should print “Buzz”
    Multiples of 7 should print “Ping”
    Multiples of 11 should print “Pong”

--- fizzbuzz/java/Zach/zjFizzBuzz.java
+++ fizzbuzz/java/Zach/zjFizzBuzz.java
@@ -4,16 +4,37 @@ public class zjFizzBuzz {
         int n = Integer.parseInt(args[0]);
 
         for(int i = 1; i <= n; i++) {
-            if(i % 3 ==0 && i % 5 == 0) {
+            if(i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0) {
+                System.out.println("FizzBuzzPingPong");
+            }else if(i % 3 == 0 && i % 5 == 0 && i % 7 == 0){
+                System.out.println("FizzBuzzPing");
+            }else if(i % 3 == 0 && i % 5 == 0 && i % 11 == 0){
+                System.out.println("FizzBuzzPong");
+            }else if(i % 3 == 0 && i % 7 == 0 && i % 11 == 0){
+                System.out.println("FizzPingPong");
+            }else if(i % 5 == 0 && i % 7 == 0 && i % 11 == 0){
+                System.out.println("BuzzPingPong");
+            }else if(i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
-            }
-            else if(i % 3 == 0) {
+            }else if(i % 3 == 0 && i % 7 == 0){
+                System.out.println("FizzPong");
+            }else if(i % 3 == 0 && i % 11 == 0){
+                System.out.println("FizzPing");
+            }else if(i % 5 == 0 && i % 7 == 0){
+                System.out.println("BuzzPing");
+            }else if(i % 5 == 0 && i % 7 == 0){
+                System.out.println("BuzzPong");
+            }else if(i % 7 == 0 && i % 11 == 0){
+                System.out.println("PingPong");
+            }else if(i % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            else if (i % 5 == 0) {
+            }else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }
-            else {
+            }else if(i % 7 == 0){
+                System.out.println("Ping");
+            }else if(i % 11 == 0){
+                System.out.println("Pong");
+            }else {
                 System.out.println(i);
             }
         }
