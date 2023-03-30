ea354cf4d0682f5600c31238935885c88cf09592
Skeegan123 <mcfeegan123@gmail.com>
2023-03-30 17:25:36 -0600

    Seperated tests into 1 for each number instead of all together

--- fizzbuzz/python/kruize/test_fizzbuzz.py
+++ fizzbuzz/python/kruize/test_fizzbuzz.py
@@ -2,10 +2,13 @@ import unittest
 from FizzBuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizzbuzz(self):
+    def test_fizz(self):
         self.assertEqual(fizzbuzz(3), 'Fizz')
+    def test_buzz(self):
         self.assertEqual(fizzbuzz(5), 'Buzz')
+    def test_ping(self):
         self.assertEqual(fizzbuzz(7), 'Ping')
+    def test_pong(self):
         self.assertEqual(fizzbuzz(11), 'Pong')
 
 if __name__ == '__main__':
