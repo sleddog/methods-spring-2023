4d34d7ff3d44a58bca99de3a9a6f479367a52087
cameronwilcox <cjwilcox15@gmail.com>
2023-03-31 12:19:35 -0600

    Split asserts out into their own tests

--- fizzbuzz/python/coryjanes/fizzbuzz_tests.py
+++ fizzbuzz/python/coryjanes/fizzbuzz_tests.py
@@ -2,10 +2,13 @@ import unittest
 from fizzbuzz import filter
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizz_buzz(self):
+    def test_three(self):
         self.assertEqual(filter(3), 'Fizz')
+    def test_five(self):
         self.assertEqual(filter(5), 'Buzz')
+    def test_seven(self):
         self.assertEqual(filter(7), 'Ping')
+    def test_eleven(self):
         self.assertEqual(filter(11),'Pong')
 
 if __name__ == "__main__":
