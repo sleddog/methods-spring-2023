1187faaaf7215a5c05775e48caa7edba0f9d50c5
AlngCameAWizard <zdjewett@gmail.com>
2023-04-04 17:25:05 -0600

    FizzBuzz++ update, now with Tests!

---
+++ fizzbuzz/java/Zach/zjFizzBuzzTests.java
@@ -0,0 +1,21 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class zjFizzBuzzTests {
+    @Test
+    public void testFizz() {
+        String[] args = {"3", "5"};
+        String expected = "1\r\n2\r\nFizz\r\n4\r\n5\r\n";
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        zjFizzBuzz.main(args);
+        String received = outContent.toString();
+        assertEquals(expected, received);
+    }
+
+    //pushing for now, more tests to come...
+    
+}
