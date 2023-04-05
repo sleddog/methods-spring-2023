cef7824533e79c1ae098071c42d7ec1a7ec3cfa1
laytonmccafferty <layton.mccafferty@gmail.com>
2023-04-04 21:48:58 -0600

    updating fizzbuzz to fizzbuzzpingpong and downloading jest for tests

--- fizzbuzz/typescript/Skeegan123/main.ts
+++ fizzbuzz/typescript/Skeegan123/main.ts
@@ -1,3 +1,5 @@
+export
+
 function main(): void {
   let arg: number = parseInt(process.argv[2], 10);
   if (isNaN(arg)) {
@@ -10,9 +12,13 @@ function main(): void {
 }
 
 function fizzbuzz(n: number): string {
+  if (n % 1155 === 0) return "FizzBuzzPingPong";
+  if (n % 77 === 0) return "PingPong";
   if (n % 15 === 0) return "FizzBuzz";
   if (n % 3 === 0) return "Fizz";
   if (n % 5 === 0) return "Buzz";
+  if (n % 7 === 0) return "Ping";
+  if (n % 11 === 0) return "Pong";
   return n.toString();
 }
 
