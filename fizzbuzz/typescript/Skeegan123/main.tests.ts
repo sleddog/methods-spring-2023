2729d8b1cfdc91b88802130263c1048dcdc96dcc
laytonmccafferty <layton.mccafferty@gmail.com>
2023-04-05 10:30:51 -0600

    adds more unit tests

--- fizzbuzz/typescript/Skeegan123/main.tests.ts
+++ fizzbuzz/typescript/Skeegan123/main.tests.ts
@@ -4,5 +4,22 @@ describe('testing fizzbuzz', () => {
   test('multiples of 3 print fizz', () => {
     expect(fizzbuzz(3).toBe(0));
   });
+  test('multiples of 5 print buzz', () => {
+    expect(fizzbuzz(5).toBe(0));
+  });
+  test('multiples of 7 print ping', () => {
+    expect(fizzbuzz(7).toBe(0));
+  });
+  test('multiples of 11 print pong', () => {
+    expect(fizzbuzz(11).toBe(0));
+  });
+  test('multiples of 15 print fizzbuzz', () => {
+    expect(fizzbuzz(15).toBe(0));
+  });
+  test('multiples of 77 print pingpong', () => {
+    expect(fizzbuzz(77).toBe(0));
+  });
+  test('multiples of 1155 print fizzbuzzpingpong', () => {
+    expect(fizzbuzz(1155).toBe(0));
+  });
 });
-
