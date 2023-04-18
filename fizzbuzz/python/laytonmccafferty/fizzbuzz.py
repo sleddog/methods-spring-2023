56e05bc868720f1b8899cf6fe2196407c7c30339
Darshan Diamond <darshandiamond.com>
2023-04-18 13:58:56 -0600

    added ping pong

--- fizzbuzz/python/laytonmccafferty/fizzbuzz.py
+++ fizzbuzz/python/laytonmccafferty/fizzbuzz.py
@@ -1,5 +1,5 @@
 import sys
-
+'''
 def fizzbuzz(maxnumber):
     count = 1;
     while count <= maxnumber:
@@ -19,3 +19,30 @@ def fizzbuzz(maxnumber):
 if __name__ == '__main__':
     userInput = int(sys.argv[1])
     fizzbuzz(userInput)
+    '''
+    #Darshan adding to Layton's code
+
+def pingpong(maxnumber):
+    count = 1;
+    while count <= maxnumber:
+        string = ""
+        if count % 3 == 0:
+            string += "Fizz"
+        if count % 5 == 0: #added code for ping pong
+            string += "Buzz"
+        if count % 7 == 0:
+            string += "Ping"
+        if count % 11 == 0:
+            string += "Pong"
+        if string != "":
+            print(string)
+            count += 1
+        else:
+            print(count)
+            count += 1
+
+
+if __name__ == '__main__':
+    userInput = int(sys.argv[1])
+    pingpong(userInput)
+
